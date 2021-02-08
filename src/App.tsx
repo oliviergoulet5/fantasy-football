6751cd78ec05affb2175939f7732cc3b4a018a40
Olivier Goulet <olivier_goulet@icloud.com>
2021-02-07 22:48:15 -0500

    Multi-thumb slider from materialUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
 import NavigationBar from './components/NavigationBar';
+import BrowserFilter from './components/BrowserFilters';
+import React, { useState } from 'react';
 
 function App() {
 
   return (
     <div className='h-screen bg-gray-100'>
       <NavigationBar />
+      <main className='h-screen'>
+        <BrowserFilter />
+      </main>
     </div>
   );
 }
---
+++ src/components/RangeSlider.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Slider from '@material-ui/core/Slider';
+import { styled } from '@material-ui/styles';
+
+type Props = {
+    min: number,
+    max: number
+}
+
+const StyledSlider = styled(Slider)({
+    color: '#1D4ED8'
+})
+
+function RangeSlider({ min, max }: Props) {
+    const [value, setValue] = React.useState<number[]>([min, max]);
+
+    const handleChange = (event: any, newValue: number | number[]) => {
+        setValue(newValue as number[]);
+    };
+    
+    return (
+        <div className='mx-2 w-60 h-8'>
+            <StyledSlider
+                value={value}
+                onChange={handleChange}
+                valueLabelDisplay="auto"
+                max={ max }
+                min={ min }
+            >
+
+            </StyledSlider>
+        </div>
+    )
+}
+
+
+
+export default RangeSlider;
