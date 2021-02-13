810dffa25392aace3ee9ae3b7f28b753c4477d7a
Olivier Goulet <olivier_goulet@icloud.com>
2021-02-13 00:41:43 -0500

    Extracted SigninButton from ProfileDropdown

--- src/components/navigationBar/ProfileDropdown.tsx
+++ src/components/navigationBar/ProfileDropdown.tsx
@@ -2,33 +2,22 @@ import mock_avi from '../../images/mock/avatar.png';
 import { useOutsideAlerter } from '../../hooks/outsideAlerter';
 import { Transition } from '@headlessui/react';
 import { useMeQuery } from '../../generated/graphql';
-import React, { useContext } from 'react';
+import React from 'react';
 import AvatarButton from './profileDropdown/avatarButton';
-import AccountModalContext from '../../contexts/AccountModalContext';
 import { Link } from 'react-router-dom';
+import SigninButton from './profileDropdown/signinButton';
 
 function ProfileDropdown() {
-    const options = ['Your profile', 'Settings', 'Sign out'];
     const { visible, setVisible, ref } = useOutsideAlerter(false);
-    const { loading: fetchingAccount, error, data: accountData } = useMeQuery();
+    const { loading: fetchingAccount, data: accountData } = useMeQuery();
     const handleProfileButtonClick = () => setVisible(!visible);
-    const { accountModalVisible, setAccountModalVisible } = useContext(
-        AccountModalContext
-    )!;
 
     let accountStateRender: JSX.Element | undefined;
 
     if (fetchingAccount) {
         // greyed out icon?
     } else if (!accountData?.me) {
-        accountStateRender = (
-            <div
-                onClick={() => { setAccountModalVisible(true) }}
-                className="button primary flex content-center h-8 px-4 leading-tight"
-            >
-                <p className="text-sm">Sign In</p>
-            </div>
-        );
+        accountStateRender = <SigninButton />
     } else {
         accountStateRender = (
             <>
