99f409bffc661cd990ec007a226ffce331c0cea7
Olivier Goulet <olivier_goulet@icloud.com>
2021-02-13 00:06:07 -0500

    Extracted search bar and logo from NavigationBar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,5 +1,3 @@
-import logo from '../images/logo.png';
-import { ReactComponent as SearchIcon } from '../images/icons/search.svg';
 import { ReactComponent as LiveIcon } from '../images/icons/live.svg';
 import MobileSidebar from './navigationBar/MobileSidebar';
 import ProfileDropdown from './navigationBar/ProfileDropdown';
@@ -8,7 +6,8 @@ import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import NavigationPageLink from './navigationBar/NavigationPageLink';
 import { MAIN_PAGES } from '../constants';
-
+import NavigationSearchBar from './navigationBar/NavigationSearchBar';
+import Logo from './navigationBar/Logo';
 function NavigationBar() {
     const [mobileSidebarVisible, setMobileSidebarVisible] = useState(false);
     const currentPage = useLocation().pathname.substring(1);
@@ -29,20 +28,7 @@ function NavigationBar() {
                         </div>
 
                         <div className="sm:items-stretch sm:justify-start flex items-center justify-center flex-1 h-full">
-                            {/* Logo */}
-                            <div className="flex items-center flex-shrink-0">
-                                <img
-                                    className="lg:hidden block w-auto h-8"
-                                    src={logo}
-                                    alt="fantasy football"
-                                />
-                                <img
-                                    className="lg:block hidden w-auto h-8"
-                                    src={logo}
-                                    alt="fantasy football"
-                                />
-                            </div>
-
+                            <Logo />
                             {/* Links */}
                             <div className="sm:block sm:ml-6 hidden">
                                 <div className="m-full flex items-center content-center h-full space-x-4">
@@ -62,18 +48,7 @@ function NavigationBar() {
                                 </div>
                             </div>
                         </div>
-
-                        {/* Search */}
-                        <div className="sm:flex focus:ring-blue-700 focus:border-blue-700 items-center flex-shrink-0 hidden w-2/12 h-8 pr-2 mr-6 border border-gray-300 rounded-md outline-none">
-                            <SearchIcon className="flex-none w-5 h-5 mx-1 text-gray-500 fill-current" />
-                            <input
-                                type="text"
-                                name="search"
-                                placeholder="Search"
-                                className="sm:text-sm w-full outline-none"
-                            ></input>
-                        </div>
-
+                        <NavigationSearchBar />
                         <ProfileDropdown />
                     </div>
                 </div>
