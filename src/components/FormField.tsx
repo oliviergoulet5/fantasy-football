05cfca55929b1187ab143a13d12c0399e176d71e
Olivier Goulet <olivier_goulet@icloud.com>
2021-03-02 04:51:45 -0500

    Extracted labels from FormField

--- src/components/FormField.tsx
+++ src/components/FormField.tsx
@@ -1,29 +1,13 @@
 import React from 'react';
 import { Field } from 'formik';
-import toTitleCase from '../utils/toTitleCase';
+import { FormFieldProps } from '../types';
+import FormLabel from './FormLabel';
 
-type Props = {
-    name: string;
-    type: 'password' | 'text';
-    errorMessage?: string;
-};
-
-function FormField({ name, type, errorMessage }: Props) {
-    const labelText = toTitleCase(name);
+function FormField({ name, type, errorMessage }: FormFieldProps) {
 
     return (
         <>
-            <div
-                className={
-                    'flex content-end justify-between' +
-                    (errorMessage !== undefined && ' ring-red-600')
-                }
-            >
-                <label htmlFor={name} className="label">
-                    {labelText}
-                </label>
-                <p className="text-xs text-red-600">{errorMessage}</p>
-            </div>
+            <FormLabel name={ name } errorMessage={ errorMessage } />
             <Field name={name} type={type} className="input-text" />
         </>
     );
