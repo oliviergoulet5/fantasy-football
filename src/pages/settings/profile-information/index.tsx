6f08c1b613c87778b956722d1914b1430e7c6124
Olivier Goulet <olivier_goulet@icloud.com>
2021-03-17 16:59:56 -0400

    Changed formik components to hooks for profile-information

--- src/pages/settings/profile-information/index.tsx
+++ src/pages/settings/profile-information/index.tsx
@@ -2,7 +2,7 @@ import { useMeProfileQuery, useUpdateAvatarMutation, useUpdateProfileMutation } 
 import { SettingsLayout, MainLayout } from '../../../common/layouts';
 import { AVATAR_DEFAULT, CLUBS } from '../../../constants';
 import { NetworkStatus } from '@apollo/client';
-import { Formik, FormikHelpers, Form } from 'formik';
+import { Formik, FormikHelpers, Form, useFormik } from 'formik';
 import { FormField, FormDropdown, FormTextArea } from '../../../common/components';
 import {toErrorMap, getChangedValues, noCache } from '../../../common/utils';
 import { validationSchema } from '../../../modules/settings/profile-information';
@@ -17,6 +17,8 @@ interface ProfileInformationFormValues {
 interface TempFormValues {
     avatar?: FileList[0]
 }
+
+const NORMAL_STATUS = undefined;
 
 const defaultValues: ProfileInformationFormValues = {
     name: '',
@@ -39,91 +41,96 @@ function ProfileInformation() {
         avatarLocation: profileData.me.avatarLocation || defaultValues.avatarLocation
     };
 
-    const updateProfileInformation = async (
-        values: ProfileInformationFormValues & TempFormValues,
-        { setSubmitting, setErrors, setStatus }: FormikHelpers<ProfileInformationFormValues>
-    ) => {
-        setSubmitting(true);
-        const profileResponse = await updateProfile({
-            variables: getChangedValues(values, initialValues)
-        });
-        console.log(values.avatarLocation);
-        if (profileResponse.data?.updateAccount.errors) {
-            setErrors(toErrorMap(profileResponse.data.updateAccount.errors));
-            setStatus('error')
-        } else if (profileResponse.data?.updateAccount.account) {
-            setStatus('success');
+    const formik = useFormik({
+        initialValues,
+        validationSchema,
+        onSubmit: async (values: ProfileInformationFormValues & TempFormValues, { setSubmitting, setErrors, setStatus }) => {
+            setSubmitting(true);
+            const profileResponse = await updateProfile({
+                variables: getChangedValues(values, initialValues)
+            });
+
+            const profileError = profileResponse.data?.updateAccount.error;
+
+            if (profileError?.fieldError) {
+                setErrors(toErrorMap([profileError.fieldError]));
+                setStatus(NORMAL_STATUS)
+            } else if (profileError?.formError) {
+                setStatus(profileError.formError.message);
+            } else {
+                setStatus(NORMAL_STATUS)
+            }
+
+            if (values.avatar) {
+                await updateAvatar({ variables: { avatar: values.avatar } });
+                refetch();
+            }
+
+            setSubmitting(false);
         }
+    });
 
-        if (values.avatar) {
-            await updateAvatar({ variables: { avatar: values.avatar } });
-            refetch();
-        }
-
-        setSubmitting(false);
-    }
-
+    const disabled = formik.isSubmitting || !formik.isValid || formik.status !== NORMAL_STATUS;
 
     return (
         <MainLayout>
             <SettingsLayout>
-                <Formik
-                    initialValues={ initialValues }
-                    onSubmit={ updateProfileInformation }
-                    validationSchema={ validationSchema }
-                > 
-                {({ values, errors, isValid, isSubmitting, setFieldValue, submitForm, status }) => {
-                    let disabled = isSubmitting || !isValid;
-                    return (
-                        <Form>
-                            <div className='px-4 py-2'>
-                                <h1 className='font-bold py-2 text-2xl'>Profile Information</h1>
-                                <div className='flex mt-6'>
-                                    <div>
-                                        <p className='label mb-2'>Avatar</p>
-                                        <div>
-                                            <img src={ noCache(profileData.me?.avatarLocation || defaultValues.avatarLocation) } alt='avatar-preview' className='rounded-full h-24' />
-                                            <p>Change picture</p>
-                                            <input type='file' name='avatar' onChange={
-                                                (event: React.ChangeEvent<HTMLInputElement>) => {
-                                                    if (!event.target.files) return;
-                                                    setFieldValue('avatar', event.target.files[0]);
-                                                }
-                                            }/>
-                                        </div>
-                                    </div>
-                                    <div className='flex flex-col space-y-4 ml-24 w-1/3'>
-                                        <FormField
-                                            name="name"
-                                            type="text"
-                                            errorMessage={errors.name}
-                                        />
-                                        <FormTextArea
-                                            name="bio"
-                                            placeholder="Maximum of 500 words"
-                                            errorMessage={errors.bio}
-                                            setFieldValue={ setFieldValue }
-                                            value={ values.bio }
-                                        />
-                                        <FormDropdown 
-                                            name='favouriteTeam' 
-                                            options={['None', ...CLUBS ]} 
-                                            errorMessage={ errors.favouriteTeam } 
-                                            setFieldValue={ setFieldValue } 
-                                            value={ values.favouriteTeam }
-                                        />
-                                        <input type='submit' value='Save' disabled={disabled} onClick={ submitForm } className={`button w-24 text-center ${
-                                            disabled ? 'primary-disabled' : 'primary'
-                                        }`}  />
-
-                                        {(status === 'success' && !disabled) && <p>Saved changes</p>}
-                                    </div>
+                <form>
+                    <div className='px-4 py-2'>
+                        <h1 className='font-bold py-2 text-2xl'>Profile Information</h1>
+                        <div className='flex mt-6'>
+                            <div>
+                                <p className='label mb-2'>Avatar</p>
+                                <div>
+                                    <img src={ noCache(profileData.me?.avatarLocation || defaultValues.avatarLocation) } alt='avatar-preview' className='rounded-full h-24' />
+                                    <p>Change picture</p>
+                                    <input type='file' name='avatar' onChange={
+                                        (event: React.ChangeEvent<HTMLInputElement>) => {
+                                            if (!event.target.files) return;
+                                            formik.setFieldValue('avatar', event.target.files[0]);
+                                        }
+                                    }/>
                                 </div>
                             </div>
-                        </Form>
-                    );
-                }}
-                </Formik>
+                            <div className='flex flex-col space-y-4 ml-24 w-1/3'>
+                                <FormField
+                                    name="name"
+                                    type="text"
+                                    errorMessage={ formik.errors.name }
+                                    value={ formik.values.name }
+                                    onChange={ formik.handleChange }
+                                />
+
+                                <FormTextArea
+                                    name="bio"
+                                    placeholder="Maximum of 500 words"
+                                    errorMessage={ formik.errors.bio }
+                                    setFieldValue={ formik.setFieldValue }
+                                    value={ formik.values.bio }
+                                    onChange={ formik.handleChange }
+                                />
+
+                                <FormDropdown 
+                                    name='favouriteTeam' 
+                                    options={['None', ...CLUBS ]} 
+                                    errorMessage={ formik.errors.favouriteTeam } 
+                                    setFieldValue={ formik.setFieldValue } 
+                                    value={ formik.values.favouriteTeam }
+                                    onChange={ formik.handleChange }
+                                />
+
+                                <button 
+                                    type='submit' 
+                                    disabled={ disabled } 
+                                    onClick={ formik.submitForm } 
+                                    className={`button w-24 text-center ${disabled ? 'primary-disabled' : 'primary'}`}
+                                >Save</button>
+
+                                {(status === 'success' && !disabled) && <p>Saved changes</p>}
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </SettingsLayout>
         </MainLayout>
     )
