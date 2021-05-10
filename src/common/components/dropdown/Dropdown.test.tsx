d900411a1c0164c3414ac4ee2ecf50e4a3c1f150
Olivier Goulet <oliviergoulet5@gmail.com>
2021-05-10 10:38:38 -0400

    minimize dropdown and select option test

--- src/common/components/dropdown/Dropdown.test.tsx
+++ src/common/components/dropdown/Dropdown.test.tsx
@@ -1,6 +1,6 @@
 import 'jest';
 import '@testing-library/jest-dom/extend-expect'
-import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+import { render, fireEvent, waitFor, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import { Dropdown } from '../Dropdown';
 
 describe('dropdown component', () => {
@@ -19,7 +19,26 @@ describe('dropdown component', () => {
 
     test('click and expand option list', async () => {
         const { getByTestId } = render(<Dropdown options={ options } liftSelectedOption={ mockLiftSelectedOptionValue } />);
-        fireEvent.click(screen.getByTestId('dropdown-div-menu'));
-        expect(screen.getByTestId('dropdown-ul-optionlist').childElementCount).toBe(options.length);
+        fireEvent.click(getByTestId('dropdown-div-menu'));
+        expect(getByTestId('dropdown-ul-optionlist').childElementCount).toBe(options.length);
+    });
+
+    test('minimize option list by clicking selected', async () => {
+        const { getByTestId } = render(<Dropdown options={ options } liftSelectedOption={ mockLiftSelectedOptionValue } />);
+        fireEvent.click(getByTestId('dropdown-p-selected'));
+        fireEvent.click(getByTestId('dropdown-p-selected'));
+
+        await waitForElementToBeRemoved(() => getByTestId('dropdown-ul-optionlist'));
+    });
+
+    test('select new option and display', async () => {
+        const optionToSelectIndex = 2;
+        const optionNotToSelect = 0;
+
+        const { getByTestId } = render(<Dropdown options={ options } liftSelectedOption={ mockLiftSelectedOptionValue } />);
+        fireEvent.click(getByTestId('dropdown-div-menu'));
+        fireEvent.click(getByTestId('dropdown-ul-optionlist').childNodes[optionToSelectIndex]);
+        expect(getByTestId('dropdown-p-selected').textContent).toBe(options[optionToSelectIndex]);
+        expect(getByTestId('dropdown-p-selected').textContent).not.toBe(options[optionNotToSelect]);
     });
 });
