12185cbc18e55e7e772b2ae050cd370528146a98
Olivier Goulet <oliviergoulet5@gmail.com>
2021-05-10 19:32:59 -0400

    Fixed sentence case utility
    Fixed an issue where all the words were capitalized at their first character.

--- src/common/utils/toSentenceCase.ts
+++ src/common/utils/toSentenceCase.ts
@@ -1,5 +1,8 @@
 export const toSentenceCase = (str: string) => {
-    let result = str.replace(/([A-Z])/g, ' $1');
-
+    let result = str
+        .replace(/([A-Z])/g, ' $1')
+        .toLowerCase()
+        .trim();
+        
     return result.charAt(0).toUpperCase() + result.slice(1);
 };
