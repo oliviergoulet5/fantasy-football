57aa30798c9849d44120e0b132218db9856a0f36
Olivier Goulet <oliviergoulet5@gmail.com>
2021-05-10 11:29:27 -0400

    PageLink tests

--- src/common/components/navigationBar/navigationPageLinks/PageLink.tsx
+++ src/common/components/navigationBar/navigationPageLinks/PageLink.tsx
@@ -11,10 +11,11 @@ export function PageLink({
     children,
 }: React.PropsWithChildren<Props>) {
     return (
-        <div className='flex flex-col justify-center h-full'>
+        <div className='flex flex-col justify-center h-full' data-testid='pagelink'>
             <div className='py-2 m-auto'>
                 <Link href={`/${page.toLowerCase()}`}>
                     <p
+                        data-testid='pagelink-p-text'
                         className={
                             (current ? 'text-black ' : 'text-gray-600 ') +
                             'hover:bg-gray-50 hover:text-black font-semibold rounded-md text-sm px-3 py-2 m-auto flex items-center cursor-pointer select-none'
@@ -24,11 +25,9 @@ export function PageLink({
                     </p>
                 </Link>
             </div>
-            <span
-                className={
-                    'h-0.5 w-10/12 flex place-self-center rounded-full' +
-                    (current && ' bg-blue-700')
-                }></span>
+            <div className='h-0.5 w-10/12 flex place-self-center rounded-full'>
+                { current && <span className='w-full h-full bg-blue-700' data-testid='pagelink-span-underline'></span>}
+            </div>
         </div>
     );
 }
