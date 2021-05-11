86b61c726c65e85e8de18b6cdf3a819fecd38451
Olivier Goulet <oliviergoulet5@gmail.com>
2021-05-11 17:10:27 -0400

    Logo test and removed redundant render

--- src/common/components/navigationBar/Logo.tsx
+++ src/common/components/navigationBar/Logo.tsx
@@ -2,12 +2,7 @@ export function Logo() {
     return (
         <div className='flex items-center flex-shrink-0'>
             <img
-                className='lg:hidden block w-auto h-8'
-                src='/images/logo.png'
-                alt='fantasy football'
-            />
-            <img
-                className='lg:block hidden w-auto h-8'
+                className='w-auto h-8'
                 src='/images/logo.png'
                 alt='fantasy football'
             />
